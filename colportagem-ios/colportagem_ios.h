2f811fb46114443f85e517bde678866f5564e76b
André Pimentel <andre.pimentel@iap.org.br>
2021-08-06 07:47:28 -0300

    Initial Commit

---
+++ colportagem-ios/colportagem_ios.h
@@ -0,0 +1,18 @@
+//
+//  colportagem_ios.h
+//  colportagem-ios
+//
+//  Created by Tatiane Pimentel on 06/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for colportagem_ios.
+FOUNDATION_EXPORT double colportagem_iosVersionNumber;
+
+//! Project version string for colportagem_ios.
+FOUNDATION_EXPORT const unsigned char colportagem_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <colportagem_ios/PublicHeader.h>
+
+
