c92828a13749916f16c06555ec6a322fce01a3e9
André Pimentel <andre.pimentel@iap.org.br>
2021-08-30 11:32:52 -0300

    fix: upload files changed

--- UI/Scenes/Main/Offer/OfferViewController.swift
+++ UI/Scenes/Main/Offer/OfferViewController.swift
@@ -1,51 +1,13 @@
 import UIKit
 
 public final class OfferViewController: UIViewController, Storyboarded {
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var searchContainerView: UIView! {
-        didSet {
-            searchContainerView.clipsToBounds = true
-            searchContainerView.layer.cornerRadius = 24
-        }
-    }
-    
-    struct Storyboard {
-        static let productCell = String(describing: ProductTableViewCell.self)
-    }
-    
     public final override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        registerNib()
     }
     
     func setup() {
         navigationController?.setNavigationBarHidden(true, animated: true)
         view.backgroundColor = Color.backgroundPrimary
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.backgroundColor = Color.backgroundPrimary
-        tableView.separatorStyle = .none
-    }
-    
-    func registerNib() {
-        let productNib = UINib(nibName: Storyboard.productCell, bundle: nil)
-        tableView.register(productNib, forCellReuseIdentifier: Storyboard.productCell)
-    }
-}
-
-extension OfferViewController: UITableViewDelegate, UITableViewDataSource {
-    public func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.productCell, for: indexPath) as! ProductTableViewCell
-        cell.selectionStyle = .none
-        return cell
     }
 }
