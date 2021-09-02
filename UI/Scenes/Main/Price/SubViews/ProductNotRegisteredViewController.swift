b8f56afef0afee6db7a4bd52a05043f7800585d3
André Pimentel <andre.pimentel@iap.org.br>
2021-09-02 10:47:33 -0300

    feat: add ProductNotRegisteredViewController

---
+++ UI/Scenes/Main/Price/SubViews/ProductNotRegisteredViewController.swift
@@ -0,0 +1,90 @@
+import UIKit
+import Presentation
+
+public final class ProductNotRegisteredViewController: UIViewController, Storyboarded {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchContainerView: UIView! {
+        didSet {
+            searchContainerView.clipsToBounds = true
+            searchContainerView.layer.cornerRadius = 24
+        }
+    }
+    var viewModel: LoadProductRegisteredByAccountViewModel? {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    public var loadProductRegisteredByAccount: ((LoadProductRegisteredByAccountRequest) -> Void)?
+    
+    struct Storyboard {
+        static let productCell = String(describing: ProductTableViewCell.self)
+    }
+    
+    public final override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        registerNib()
+        fetchProductRegisteredByAccount()
+    }
+    
+    func setup() {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        view.backgroundColor = Color.backgroundPrimary
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = Color.backgroundPrimary
+        tableView.separatorStyle = .none
+    }
+    
+    func registerNib() {
+        let productNib = UINib(nibName: Storyboard.productCell, bundle: nil)
+        tableView.register(productNib, forCellReuseIdentifier: Storyboard.productCell)
+    }
+    
+    func fetchProductRegisteredByAccount() {
+        loadProductRegisteredByAccount?(LoadProductRegisteredByAccountRequest(limit: 10, offSet: 0, search: "%"))
+    }
+}
+
+extension ProductNotRegisteredViewController: UITableViewDelegate, UITableViewDataSource {
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.numberOfItemsInSection(section) ?? 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.productCell, for: indexPath) as! ProductTableViewCell
+        cell.selectionStyle = .none
+        let product = self.viewModel?.productByIndex(indexPath.row)
+        cell.product = product
+        return cell
+    }
+}
+
+extension ProductNotRegisteredViewController: LoadProductRegisteredByAccountResultViewModel {
+    public func result(_ viewModel: LoadProductRegisteredByAccountViewModel) {
+        self.viewModel = viewModel
+    }
+}
+
+extension ProductNotRegisteredViewController: LoadingView {
+    public func display(viewModel: LoadingViewModel) {
+        if viewModel.isLoading {
+            view.isUserInteractionEnabled = false
+        } else {
+            view.isUserInteractionEnabled = true
+        }
+    }
+}
+
+extension ProductNotRegisteredViewController: AlertView {
+    public func showMessage(viewModel: AlertViewModel) {
+        let alert = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+}
