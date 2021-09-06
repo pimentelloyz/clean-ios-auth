8489c1273bfb1c0886da86861b0666caef60ad3e
André Pimentel <andre.pimentel@iap.org.br>
2021-09-06 14:48:29 -0300

    feat: add AddSignatureValue use case

---
+++ Domain/UseCases/Product/AddSignatureValue.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public protocol AddSignatureValue {
+    typealias Result = Swift.Result<NoContentModel, DomainError>
+    func add(with params: AddSignatureValueParameters, completion: @escaping (Result) -> Void)
+}
+
+public struct AddSignatureValueParameters: Model {
+    public let productId: Int
+    public let signatureItems: [SignatureItemsParameters]
+    
+    public init(productId: Int,
+                signatureItems: [SignatureItemsParameters]) {
+        self.productId          = productId
+        self.signatureItems     = signatureItems
+    }
+}
+
+public struct SignatureItemsParameters: Model {
+    public let monthCount: Int
+    public let salesValue: Double
+    
+    public init(monthCount: Int,
+                salesValue: Double) {
+        self.monthCount     = monthCount
+        self.salesValue     = salesValue
+    }
+}
